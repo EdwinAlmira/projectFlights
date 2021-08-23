fcccee9a3537d1f01b5f2306f9f8a9cd3535a8bd
Richard Amaya <amayaricardo2607@gmail.com>
2021-08-22 18:45:10 -0600

    el buen richard

--- src/main/java/com/ProjectFlights/ProjectFlightsApplication.java
+++ src/main/java/com/ProjectFlights/ProjectFlightsApplication.java
@@ -2,6 +2,8 @@ package com.ProjectFlights;
 
 public class ProjectFlightsApplication {
     public static void main(String[] args) {
+
         System.out.println("Working");
+        System.out.println("Working Richard");
     }
 }
